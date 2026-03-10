c55d0501df5a9f85f5d17122bdb066d031a6ae80
Mariam982440 <mariam1elhaimer@gmail.com>
2026-03-10 11:11:57 +0000

    feat : création de la migration reservations

--- database/migrations/2026_03_10_110402_create_stations_table.php
+++ database/migrations/2026_03_10_110402_create_stations_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('stations', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->foreignId('connector_type_id')->constrained()->onDelete('cascade');
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->integer('power_kw'); // puissance en kW
+            $table->enum('status', ['available', 'maintenance'])->default('available');
             $table->timestamps();
         });
     }

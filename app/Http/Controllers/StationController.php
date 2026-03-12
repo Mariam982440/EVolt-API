dcf0c23206f06e6636174c62823927de4401fde0
Mariam982440 <mariam1elhaimer@gmail.com>
2026-03-12 11:15:07 +0000

    feat : ajout de la methode index avec les filtres / sationController

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -8,5 +8,21 @@ use Illuminate\Support\Str;
 
 class StationController extends Controller
 {
+    public function index(Request $request ){
+        $query = Station::with('connectorType');
+        
+        if($request->has('connector_type_id')){
+            $query->where('connector_type_id',$request->connector_type_id);
+        }
+        if($request->has('min_power')){
+            $query->where('power_kw', '>=', $request->min_power);
+        }
+        if ($request->has('available') && $request->available == 'true') {
+            $query->where('status', 'available');
+        }
+
+        return response()->json($query->get(),200);
+
+    }
     
 }

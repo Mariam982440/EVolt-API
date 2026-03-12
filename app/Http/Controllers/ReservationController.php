40292298fffc2ca009b3c1017ca02ba8e1752692
Mariam982440 <mariam1elhaimer@gmail.com>
2026-03-12 22:33:41 +0000

    feat : creation de la fonction index /ReservationController

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Reservation;
+
 
 class ReservationController extends Controller
 {
-    //
+    public function index($request){
+        $reservations = $request->user()->reservations()
+            ->with('station.connectorType')
+            ->orderBy('start_time', 'desc')
+            ->get();
+
+        return response()->json($reservations);
+    }
+    
 }

51bd95822e23cbdeb151c4517fc2ffc70ffd180f
Mariam982440 <mariam1elhaimer@gmail.com>
2026-03-12 11:14:14 +0000

    feat : creation du controller stationController

---
+++ app/Http/Controllers/StationController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Station;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class StationController extends Controller
+{
+    
+}
